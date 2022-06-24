3dccae36b66322ed93e99b434b456addda762da9
Paulo Guerra <paulo.guerra@verndale.com>
2022-06-24 07:22:02 -0500

    :sparkles: - unocss and carbon icons

--- app.vue
+++ app.vue
@@ -1,6 +1,35 @@
 <template>
   <div>
-    <NuxtWelcome />
-    <NuxtWelcome />
+    <div class="i-carbon-sun" />
+    <div class="scroll-tester"></div>
+    <div class="i-carbon-moon" />
   </div>
 </template>
+
+<style>
+.i-carbon-sun {
+  color: gold;
+  margin-inline: auto;
+}
+.i-carbon-moon {
+  color: blue;
+  margin-inline: auto;
+}
+.scroll-tester {
+  min-block-size: 400vh;
+  inline-size: 10px;
+  border: 1px solid darkblue;
+  border-radius: 5px;
+  margin: 20px auto;
+  background-image: linear-gradient(
+    to bottom,
+    yellow,
+    gold,
+    orange,
+    orangered,
+    purple,
+    blue,
+    darkblue
+  );
+}
+</style>
